c1e3ac8729a9472ce3bb8040938174e63c8be181
Alamin Khan <alamin@Alamins-Mini.lan>
2022-08-15 17:56:59 +0300

    api

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ async function run() {
     try {
         await client.connect();
         const serviceCollection = client.db('geniusCar').collection('service');
+        const orderCollection = client.db('geniusCar').collection('order');
 
         app.get('/service', async (req, res) => {
             const query = {};
@@ -29,7 +30,7 @@ async function run() {
         });
         app.get('/service/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id: ObjectId(id) };
+            const query = { _id: ObjectId(id) };
             const service = await serviceCollection.findOne(query);
             res.send(service);
         });
@@ -40,13 +41,27 @@ async function run() {
             const result = await serviceCollection.insertOne(newService);
             res.send(result);
         });
-           // DELETE
-           app.delete('/service/:id', async(req, res) =>{
+        // DELETE
+        app.delete('/service/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id: ObjectId(id)};
+            const query = { _id: ObjectId(id) };
             const result = await serviceCollection.deleteOne(query);
             res.send(result);
         });
+        //order collection 
+        app.get('/order',async(req , res)=>{
+            const email = req.query.email;
+            const query = {email:email};
+            const cursor = orderCollection.find(query);
+            const orders = await cursor.toArray();
+            res.send(orders);
+        })
+
+        app.post('/order', async (req, res) => {
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
+            res.send(result);
+        })
 
 
     }
